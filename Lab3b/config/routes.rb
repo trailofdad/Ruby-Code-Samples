54867c5080ae52b24957a8398423991aa8f57c55
trailofdad <christian.hapgood@gmail.com>
2016-02-16 12:25:09 -0400

    have crud working... had to delete the devise code and migration as it was throwing an error.. will re-try

--- Lab3b/config/routes.rb
+++ Lab3b/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :employees
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :employees
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'employees#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
