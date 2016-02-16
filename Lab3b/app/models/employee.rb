5fe2adc5d508c6573ff444c7a7b80ad145b25af6
trailofdad <christian.hapgood@gmail.com>
2016-02-16 11:28:20 -0400

    added devise and migrated the db

--- Lab3b/app/models/employee.rb
+++ Lab3b/app/models/employee.rb
@@ -1,4 +1,8 @@
 class Employee < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 
     self.primary_key = :emp_no
 
