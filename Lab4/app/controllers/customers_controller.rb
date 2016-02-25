3c471935169b4123e0c62e97d8c5835458935d5a
trailofdad <christian.hapgood@gmail.com>
2016-02-25 15:25:22 -0400

    added pagination

--- Lab4/app/controllers/customers_controller.rb
+++ Lab4/app/controllers/customers_controller.rb
@@ -4,7 +4,7 @@ class CustomersController < ApplicationController
   # GET /customers
   # GET /customers.json
   def index
-    @customers = Customer.all
+    @customers = Customer.paginate(:page => params[:page], :per_page => 25)
   end
 
   # GET /customers/1
